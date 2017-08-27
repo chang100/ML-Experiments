c974d56de59065e4b2e126a3f9624b07d2f25717
chang100 <mail2joseph@gmail.com>
2017-08-26 17:56:42 -0700

    Bug fix to create_cifar.py

--- utils/loaders/create_cifar.py
+++ utils/loaders/create_cifar.py
@@ -17,21 +17,21 @@ train_labels = []
 
 # Get training data
 for i in range(5):
-    file = 'cifar-10-batches-py/data_batch_' + str(i + 1)
+    file = 'datasets/cifar-10-batches-py/data_batch_' + str(i + 1)
     data_batch = unpickle(file)
    
     train_data.append(data_batch['data'])
     train_labels.append(data_batch['labels'])
 
 # Get test data
-test_batch  = unpickle('cifar-10-batches-py/test_batch')
+test_batch  = unpickle('datasets/cifar-10-batches-py/test_batch')
 test_data   = test_batch['data']
 test_labels = test_batch['labels']
 
 # Get npz arrays for train and test data
-train_data   = np.concatenate(train_data).reshape(50000, 32, 32, 3)
+train_data   = np.transpose(np.concatenate(train_data).reshape(50000, 3, 32, 32), axes=[0,2,3,1])
 train_labels = np.concatenate(train_labels)
-test_data    = test_data.reshape(10000, 32, 32, 3)
+test_data    = np.transpose(test_data.reshape(10000, 3, 32, 32), axes=[0,2,3,1])
 
 # Save out train/test data and labels
 np.savez('cifar-10', train_data=train_data, test_data=test_data, train_labels=train_labels, test_labels=test_labels)
