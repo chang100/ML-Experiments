0a8628482ab675dd3b371de7060f199da62a452d
chang100 <mail2joseph@gmail.com>
2017-09-12 14:17:59 -0700

    parser reads from files

--- utils/parser.py
+++ utils/parser.py
@@ -1,7 +1,15 @@
 import argparse
+import pdb
+
+class LoadFromFile (argparse.Action):
+    def __call__ (self, parser, namespace, values, option_string = None):
+        with values as f:
+            parser.parse_args(f.read().split(), namespace)
 
 def parse():
     parser = argparse.ArgumentParser()
+    #parser.add_argument("-c", "--c", type=str, help="configuration file")
+    parser.add_argument("-c", "--c", type=open, action=LoadFromFile)
     parser.add_argument("-lr", "--lr", type=float, help="learning rate")
     parser.add_argument("-bsz", "--bsz", type=int, help="batch size")
     parser.add_argument("-epochs", "--epochs", type=int, help="number epochs")
